48fcbc3a848675344aa7ed4d1a538176e70d17d8
Alexandr <alexandrsinko2000@gmail.com>
2023-05-01 15:02:05 +0300

    task 17

---
+++ src/arroganteIT/leetCode/climbStairs/Solution.java
@@ -0,0 +1,15 @@
+package arroganteIT.leetCode.climbStairs;
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int x = 1, y = 2;
+        for(int i = 2; i < n; i ++) {
+            int temp = x;
+            x = y;
+            y += temp;
+        }
+        return y;
+    }
+}
