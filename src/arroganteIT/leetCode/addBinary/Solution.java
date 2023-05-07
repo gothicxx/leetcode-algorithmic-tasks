f7396a7bb4b0c0483904359ac0f930ecc76732a8
Alexandr <alexandrsinko2000@gmail.com>
2023-05-07 22:16:01 +0300

    initial commit

--- src/arroganteIT/leetCode/addBinary/Solution.java
+++ src/arroganteIT/leetCode/addBinary/Solution.java
@@ -3,12 +3,12 @@ package arroganteIT.leetCode.addBinary;
 import java.math.BigInteger;
 
 public class Solution {
-    public String addBinary(String a, String b) {
-
-        BigInteger n1 = new BigInteger(a, 2);
-        BigInteger n2 = new BigInteger(b, 2);
-
-        Long result = n1.longValue() + n2.longValue();
-        return Long.toBinaryString(result);
-    }
+//    public String addBinary(String a, String b) {
+//
+//        BigInteger n1 = new BigInteger(a, 2);
+//        BigInteger n2 = new BigInteger(b, 2);
+//
+//        Long result = n1.longValue() + n2.longValue();
+//        return Long.toBinaryString(result);
+//    }
 }
