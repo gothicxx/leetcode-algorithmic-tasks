84fd1ddc2a49c08a8f042c0325744efaec566c76
Alexandr <alexandrsinko2000@gmail.com>
2023-05-11 11:09:17 +0300

    singleton pattern

---
+++ src/arroganteIT/joshua_bloch__book_practice/Singleton/Singleton.java
@@ -0,0 +1,21 @@
+package arroganteIT.joshua_bloch__book_practice.Singleton;
+
+import java.io.Serializable;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    public String value;
+
+    private Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
